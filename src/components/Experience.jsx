aa97f91b08b2a89df1141adfc10b5314391dc2fb
leemj9812 <dlaudwo087@gmail.com>
2023-10-11 17:25:42 +0900

    no message

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,6 +4,13 @@ import { Bull } from "./obj/Bull";
 import { Cow } from "./obj/Cow";
 import { useRef } from "react";
 import { Deer } from "./obj/Deer";
+import { Donkey } from "./obj/Donkey";
+import { Fox } from "./obj/Fox";
+import { Horse } from "./obj/Horse";
+import { Husky } from "./obj/Husky";
+import { Stag } from "./obj/Stag";
+import { Wolf } from "./obj/Wolf";
+import { WhiteHorse } from "./obj/WhiteHorse";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from 'three'
 import { Road } from "./obj/Road";
@@ -48,10 +55,18 @@ export const Experience = () => {
     <PerspectiveCamera ref={camera} near={10} far={1000} position={[-0, 10, -40]} makeDefault></PerspectiveCamera>
       <OrbitControls />
       <Road position={[0, -5, 113]} scale={5}/>
+      <Stag position={[-18, 0, 0]}/>
+      <Horse position={[-15, 0, 0]}/>
+      <Husky position={[-11.5, 0, 0]}/>
+      <Wolf position={[-7.5, 0, 0]}/>
+      <WhiteHorse position={[-3.5, 0, 0]}/>
       <Shiba />
-      <Bull position={[4, 0, 0]} />
-      <Cow position={[8, 0, 0]} />
-      <Deer position={[12, 0, 0]}/>
+      <Fox position={[3.5, 0, 0]}/>
+      <Bull position={[7.5, 0, 0]} />
+      <Cow position={[11.5, 0, 0]} />
+      <Deer position={[15, 0, 0]}/>
+      <Donkey position={[18, 0, 0]}/>
+      
     </>
   );
 };
